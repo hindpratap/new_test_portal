60b672e56ccfb116c20054076472a3d039480ae7
Anubhav Kumar Singh <asingh2@Dataflow.biz>
2020-01-02 13:52:59 +0530

    Home page changes made

--- static/js/tab.js
+++ static/js/tab.js
@@ -6,23 +6,43 @@ const backSpan = document.querySelector('.back-span');
 const drag = document.querySelector('.drag');
 const normal = document.querySelector('.normal');
 
+const pending = document.querySelector('.pending');
+const shortlisted = document.querySelector('.shortlisted');
+
 let tabId;
+
 [...tabItems].map(e => {
    e.addEventListener('click', () => {
         tabId = e.getAttribute('data-id');
-        if(tabId === 'bulk'){
-            backSpan.style = 'left: 121px; transition: left .3s ease';
-            drag.classList.remove('hidden');
-            normal.classList.add('hidden');
-        }else if (tabId === 'single'){
-            backSpan.style = 'left: 4px; transition: left .3s ease';
-            drag.classList.add('hidden');
-            normal.classList.remove('hidden');
-        }
-        else {
-            backSpan.style = 'left: 4px; transition: left .3s ease';
-            drag.classList.add('hidden');
-            normal.classList.remove('hidden');
+        try{
+            if(tabId === 'bulk'){
+                backSpan.style = 'left: 121px; transition: left .3s ease';
+                drag.classList.remove('hidden');
+                normal.classList.add('hidden');
+            }else if (tabId === 'single'){
+                backSpan.style = 'left: 4px; transition: left .3s ease';
+                drag.classList.add('hidden');
+                normal.classList.remove('hidden');
+            }
+            else {
+                backSpan.style = 'left: 4px; transition: left .3s ease';
+                drag.classList.add('hidden');
+                normal.classList.remove('hidden');
+            }
+        }catch(err){
+            if(tabId === 'shortlisted'){
+                backSpan.style = 'left: 98px; transition: left .3s ease';
+                shortlisted.classList.remove('hidden');
+                pending.classList.add('hidden');
+            }else if(tabId === 'pending'){
+                backSpan.style = 'left: -2px; transition: left .3s ease';
+                shortlisted.classList.add('hidden');
+                pending.classList.remove('hidden');
+            }else{
+                backSpan.style = 'left: -2px; transition: left .3s ease';
+                shortlisted.classList.add('hidden');
+                pending.classList.remove('hidden');
+            }
         }
    });
 });
